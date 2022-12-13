6b17fa8e83dc4e4adf133d499f20c7a62bdf142e
Vitali <vitali.75@mail.ru>
2022-12-13 14:39:42 +0300

    2 новые задачи

---
+++ Program17.cs
@@ -0,0 +1,17 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите числа через пробел!");
+int[] array = Console.ReadLine()!.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int GetPositivNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i] > 0)
+    {
+        result = result + 1;
+    }
+    return result;
+}
+Console.WriteLine($"Чисел больше 0:  {GetPositivNumbers(array)} !");
---
+++ Program18.cs
@@ -0,0 +1,11 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите значения двух прямых!");
+double b1 = double.Parse(Console.ReadLine()!);
+double k1 = double.Parse(Console.ReadLine()!);
+double b2 = double.Parse(Console.ReadLine()!);
+double k2 = double.Parse(Console.ReadLine()!);
+double x = (b1 - b2) / (k1 - k2);
+double y = (k2*b1-k1*b2)/(k2-k1);
+Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
