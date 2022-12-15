17cff1b0b5dd38b1ac058ed00d03eb5dabd3d965
Vitali75P <119442675+Vitali75P@users.noreply.github.com>
2022-12-15 20:41:40 +0300

    Проверено!

--- Program18.cs
+++ Program18.cs
@@ -9,3 +9,4 @@ double k2 = double.Parse(Console.ReadLine()!);
 double x = (b1 - b2) / (k1 - k2);
 double y = (k2*b1-k1*b2)/(k2-k1);
 Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+//
